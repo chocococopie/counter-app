382de73c4e2dea028ebaf818bc47b6ff816376a9
Diana Shagiakhmetova <dshagiakh@gmail.com>
2021-04-29 21:25:37 +0200

    style: add styles for counter component

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,21 +1,33 @@
 import React, { useState } from 'react'
+import "./Counter.css";
 
 function Counter() {
-
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="container">
       <h1>Counter</h1>
-      <div>
+      <div className="display">
         <span>{count}</span>
       </div>
-      <div>
-        <button onClick={()=>setCount(count+1)}>+</button>
-        <button onClick={()=>setCount(count-1)}>-</button>
-        <button onClick={()=>setCount(0)}>Reset</button>
+      <div className="button-group">
+        <button
+          className="button increment"
+          onClick={() => setCount(count + 1)}
+        >
+          +
+        </button>
+        <button
+          className="button decrement"
+          onClick={() => setCount(count - 1)}
+        >
+          -
+        </button>
+        <button className="button reset" onClick={() => setCount(0)}>
+          Reset
+        </button>
       </div>
-    </>
+    </div>
   );
 }
 
